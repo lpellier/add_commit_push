9c8d5d55165deb7e57c0bfe03a50fc8732859e8e
lpellier <pellierlucas@gmail.com>
2021-04-20 15:30:43 +0200

    consistency

--- acp.sh
+++ acp.sh
@@ -7,18 +7,18 @@ RESET="\e[0m"
 BRANCH="$1";
 if [ -z "$BRANCH" ]
 then
-	printf 'Please run the following command : \nacp "branch"\n'
+	printf "${BLUE}Please run the following command : \nacp \"branch\"\n${RESET}"
 	exit
 fi
 
 if [ -e "$PWD/Makefile" ]
 then
-	printf "${BLUE}%s\n${RESET}" "Makefile found, cleaning up..."
+	printf "${BLUE}Makefile found, cleaning up...${RESET}\n"
 	make fclean > /dev/null
 fi
 git add $PWD
 git status
-printf "${BLUE}%s${RED}\n" "Confirm push ? (y/n)"
+printf "${BLUE}Confirm push ? (y/n)${RED}\n"
 read answer
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ]
 then
